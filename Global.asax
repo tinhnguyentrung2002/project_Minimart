0c73c4ae5ce2a03cfee031bc3304e1ab61557c9e
Trung Nguyễn <tinh.nguyentrung2002@gmail.com>
2023-07-23 16:37:27 +0700

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LTW_NTTinh_2024802010018.MvcApplication" Language="C#" %>
---
+++ script.js
@@ -0,0 +1,17 @@
+  
+//*-----------------*//
+
+var loader = function() {
+    setTimeout(function() {
+        $('#loader').css({'opacity': 0.7, 'visibility': 'hidden' });
+    }, 800);
+};
+$(function(){
+    loader();
+});
+
+//*-----------------*
+
+//*-----------------*//
+
+
